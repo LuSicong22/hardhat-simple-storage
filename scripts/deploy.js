c068cc96ac2cd9ad7a2bfcad017176ff90306acd
LuSicong22 <lusicong22@gmail.com>
2022-08-02 11:09:51 +0800

    init

---
+++ scripts/deploy.js
@@ -0,0 +1,47 @@
+// imports
+const { ethers, network } = require("hardhat")
+
+// async main
+async function main() {
+    const SimpleStorageFactory = await ethers.getContractFactory(
+        "SimpleStorage"
+    )
+    console.log("Deplying contract...")
+    const simpleStorage = await SimpleStorageFactory.deploy()
+    await simpleStorage.deployed()
+    console.log(`Deployed contract to: ${simpleStorage.address}`)
+    // what happens when we deploy to our hardhat network?
+    if (network.congig.chainId === 4 && process.env.ETHERSCAN_API_KEY) {
+        await simpleStorage.deployTransaction.wait(6)
+        await verify(simpleStorage.address, [])
+    }
+    const currentValue = await simpleStorage.retrieve()
+    console.log(`Current Value is: ${currentValue}`)
+
+    // Update the current value
+    const transactionResponse = await simpleStorage.store(7)
+    await transactionResponse.wait(1)
+    const updatedValue = await simpleStorage.retrieve()
+    console.log(`Updated Value is: ${updatedValue}`)
+
+}
+
+async function verify(contractAddress, args) {
+    console.log("Verifying contract...")
+    try {
+        await run ("verify:verify",{
+            address: contractAddress,
+            constructorArguments: args,
+        })
+    } catch (e) {
+        
+    }
+}
+
+// main
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
